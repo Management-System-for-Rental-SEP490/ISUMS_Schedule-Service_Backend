b4f71950c0a47a40a3c0bd94d3e8186140337dbe
minhduy <MDuy10x203@gmail.com>
2026-03-16 17:52:03 +0700

    remove CORS config

--- src/main/java/com/isums/scheduleservice/infrastructures/abstracts/LeaveRequestService.java
+++ src/main/java/com/isums/scheduleservice/infrastructures/abstracts/LeaveRequestService.java
@@ -4,9 +4,11 @@ import com.isums.scheduleservice.domains.dtos.CreateLeaveRequest;
 import com.isums.scheduleservice.domains.dtos.LeaveRequestDto;
 import com.isums.scheduleservice.domains.dtos.UpdateLeaveStatusRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LeaveRequestService {
     LeaveRequestDto createLeaveRequest(CreateLeaveRequest req);
     LeaveRequestDto updateStatus(UUID leaveId, UpdateLeaveStatusRequest request);
+    List<LeaveRequestDto> getLeaveRequestByStaffId(UUID staffId);
 }
