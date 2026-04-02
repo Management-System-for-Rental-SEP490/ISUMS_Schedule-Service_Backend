1e5d7e3a09248ced25e6066da491d0fc064c677c
minhduy <MDuy10x203@gmail.com>
2026-04-02 10:28:01 +0700

    add inspection

--- src/main/java/com/isums/scheduleservice/domains/dtos/ManualAssignRequest.java
+++ src/main/java/com/isums/scheduleservice/domains/dtos/ManualAssignRequest.java
@@ -1,11 +1,14 @@
 package com.isums.scheduleservice.domains.dtos;
 
+import com.isums.scheduleservice.domains.enums.JobType;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record ManualAssignRequest(
         UUID jobId,
         UUID staffId,
-        LocalDateTime startTime
+        LocalDateTime startTime,
+        JobType jobType
 ) {
 }
